8924c075d658d4a769fb733a2250ae8334eb47aa
FabioLuisDias <f.araujoo@hotmail.com>
2022-03-31 10:14:23 -0300

    V1.0

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Cliente fabio = new Cliente();
+        fabio.setNome("Fabio");
+
+        Conta cc = new ContaCorrente(fabio);
+        Conta poupanca = new ContaPoupanca(fabio);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+}
