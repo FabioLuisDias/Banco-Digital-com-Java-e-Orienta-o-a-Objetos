399e410a1b6877daeefa410c52af137a73a9c01e
FabioLuisDias <f.araujoo@hotmail.com>
2022-04-04 01:29:13 -0300

    V1.0

--- src/Banco.java
+++ src/Banco.java
@@ -1,10 +1,9 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
 
     private String nome;
-    private List<Conta> contas;
-
 
     public String getNome() {
         return nome;
@@ -13,19 +12,4 @@ public class Banco {
     public void setNome(String nome) {
         this.nome = nome;
     }
-
-    public List<Conta> getContas() {
-        return contas;
-    }
-
-    public void setContas(List<Conta> contas) {
-        this.contas = contas;
-    }
-
-//    protected void imprimirContas() {
-//        for (Conta contas: contas) {
-//            System.out.println(contas);
-//            System.out.println(String.format("Agencia: %d", this.conta));
-//        }
-//    }
 }
---
+++ src/Funcionario.java
@@ -0,0 +1,26 @@
+
+
+public class Funcionario extends Pessoa {
+
+    private String funcao;
+    private Double salario;
+
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+}
+
